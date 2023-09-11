aaa50c62354f551ad18bfb218db489082eaa33ec
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2023-09-11 18:51:03 -0300

    F

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,24 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function validateForm() {
+    var name = document.forms["contact"]["name"].value;
+    var email = document.forms["contact"]["email"].value;
+    var message = document.forms["contact"]["message"].value;
+
+    if (name === "") {
+        alert("Name must be filled out.");
+        return false;
+    }
+    if (email === "") {
+        alert("Email must be filled out.");
+        return false;
+    }
+    if (message === "") {
+        alert("Message must be filled out.");
+        return false;
+    }
+
+    return true;
+}
